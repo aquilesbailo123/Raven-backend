8dd1674d25ef3521ae297c58e3e2287a91f16509
seo-onion <sebastianhernandezminano@gmail.com>
2025-12-03 15:57:10 -0500

    push

--- build_scripts/render.sh
+++ build_scripts/render.sh
@@ -3,15 +3,16 @@
 set -o errexit
 
 # Initial setup
-python -m pip install --upgrade pip
+source venv/bin/activate
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run migrations
-python manage.py migrate
-python wizard.py
+python3 manage.py migrate
+python3 wizard.py
 
 # mkdir -p staticfiles
-python manage.py collectstatic
+python3 manage.py collectstatic
 
 # Build command
 # gunicorn backend.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
