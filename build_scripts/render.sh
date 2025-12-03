94526a1faeeb57836360db81d0fa7561255b2d11
seo-onion <sebastianhernandezminano@gmail.com>
2025-12-03 16:07:47 -0500

    push

--- build_scripts/render.sh
+++ build_scripts/render.sh
@@ -2,17 +2,14 @@
 # Exit on error
 set -o errexit
 
-# Initial setup
-source venv/bin/activate
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
-
 # Run migrations
 python3 manage.py migrate
+
+# Run wizard.py
 python3 wizard.py
 
-# mkdir -p staticfiles
-python3 manage.py collectstatic
+# Collect static files for Django
+python3 manage.py collectstatic --noinput
 
-# Build command
+# Build command (commented out from original)
 # gunicorn backend.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
